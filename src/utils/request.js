2f064ebd2f68bacc9b5e0d8e01ddd39dec8f1702
extends <7646687+extendsx@user.noreply.gitee.com>
2022-08-20 23:19:29 +0800

    登录验证

--- src/utils/request.js
+++ src/utils/request.js
@@ -47,27 +47,28 @@ service.interceptors.response.use(
     const res = response.data
 
     // if the custom code is not 20000, it is judged as an error.
-    if (res.code !== 20000) {
+    console.log(res.success);
+    if (!res.success) {
       Message({
-        message: res.message || 'Error',
+        message: res.message || 'Error1',
         type: 'error',
         duration: 5 * 1000
       })
 
       // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
-      if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
-        // to re-login
-        MessageBox.confirm('You have been logged out, you can cancel to stay on this page, or log in again', 'Confirm logout', {
-          confirmButtonText: 'Re-Login',
-          cancelButtonText: 'Cancel',
-          type: 'warning'
-        }).then(() => {
-          store.dispatch('user/resetToken').then(() => {
-            location.reload()
-          })
-        })
-      }
-      return Promise.reject(new Error(res.message || 'Error'))
+      // if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
+      //   // to re-login
+      //   MessageBox.confirm('You have been logged out, you can cancel to stay on this page, or log in again', 'Confirm logout', {
+      //     confirmButtonText: 'Re-Login',
+      //     cancelButtonText: 'Cancel',
+      //     type: 'warning'
+      //   }).then(() => {
+      //     store.dispatch('user/resetToken').then(() => {
+      //       location.reload()
+      //     })
+      //   })
+      // }
+      return Promise.reject(new Error(res.message || 'Error2'))
     } else {
       return res
     }
@@ -76,7 +77,7 @@ service.interceptors.response.use(
     console.log('err' + error) // for debug
     Message({
       message: error.message,
-      type: 'error',
+      type: 'error4',
       duration: 5 * 1000
     })
     return Promise.reject(error)
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -110,7 +110,6 @@ export default {
         if (valid) {
           this.loading = true
           this.$store.dispatch('user/login', this.loginForm).then(() => {
-            console.log(1111);
             this.$router.push({ path: this.redirect || '/' })
             this.loading = false
           }).catch(() => {
